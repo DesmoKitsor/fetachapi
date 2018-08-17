2cef7648c1f0d8594303e1f8171ec405729becda
DesmoKitsor <desmokitsor@gmail.com>
2018-08-17 12:19:00 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: null,
+    }
+  }
+
+  componentDidMount () {
+
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then ( (response) => response.json() )
+      .then ( (responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        })
+
+      })
+
+      .catch((error) => {
+        console.log(error)
+      });
+  }
+
+  render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      )
+    } else {
+
+      let movies = this.state.dataSource.map((val, key) => {
+        return <View key={key} style={styles.item}>
+                  <Text>{val.title}</Text>
+               </View>
+      });
+
+      return (
+        <View style={styles.container}>
+          {movies}
+        </View>
+      );
+    }  
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'skyblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+item: {
+    flex: 1,
+    alignSelf: 'stretch',
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black'
+}
+});
